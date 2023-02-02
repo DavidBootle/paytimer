97abd37a0e98e85944875672c026356ead3bac79
David Bootle <davidtbootle@gmail.com>
2023-02-02 12:40:26 -0500

    Finished rate container

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// TEMP
+// SHOW OR HIDE PAUSE BUTTON
 let timerActive = false;
 
 $("#resetbutton").show();
@@ -10,3 +10,13 @@ if (timerActive) {
     $("#pausebutton").hide();
     $("#playbutton").show();
 }
+
+// RESIZE RATE INPUT TO FIT CONTENT
+function resizeRateInput() {
+    let input = $("#rate-amount-input");
+    let hidden = $("#hidden");
+    hidden.text(input.val());
+    input.width(hidden.width());
+}
+$("#rate-amount-input").on('input', (e) => { resizeRateInput(); });
+resizeRateInput();
